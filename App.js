c4e032a51f2e1571674ee5e554dea80f2df0e2cb
Cesron <cesronvasquez@gmail.com>
2022-10-11 22:39:18 -0600

    Stack Navigation added

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import * as eva from "@eva-design/eva";
-import {
-	ApplicationProvider,
-	Button,
-	Layout,
-	Text,
-} from "@ui-kitten/components";
-import React from "react";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BienvenidaScreen } from './screens/BienvenidaScreen';
+import { LoginScreen } from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<ApplicationProvider {...eva} theme={eva.dark}>
-			<Layout style={styles.layout}>
-				<Text category="h1">La Tienda De La Abuela</Text>
-				<Button color="white">¡Empezar!</Button>
-			</Layout>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName='Home'>
+					<Stack.Screen name='Bienvenida' component={BienvenidaScreen} />
+					<Stack.Screen name='Login' component={LoginScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
 		</ApplicationProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	layout: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
---
+++ screens/BienvenidaScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, Layout, Text } from '@ui-kitten/components';
+
+export function BienvenidaScreen({ navigation }) {
+	return (
+		<Layout style={styles.layout}>
+			<Text category='h1'>La Tienda De La Abuela</Text>
+			<Button onPress={() => navigation.navigate('Login')}>¡Empezar!</Button>
+		</Layout>
+	);
+}
+
+const styles = StyleSheet.create({
+	layout: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
