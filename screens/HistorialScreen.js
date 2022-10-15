06b65b540bcf5ab3fc82e8b6f06a31a1409bc858
LiltLopez <111619197+LiltLopez@users.noreply.github.com>
2022-10-14 20:51:08 -0600

    Added conexion, historial and pedidos screen

---
+++ screens/HistorialScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity,Image} from "react-native";
+
+const HistorialScreen = () => {
+
+	return (
+		<View style={styles.contenedor}>
+			<View style={styles.iconoContenedor}>
+				<Image style={styles.icono} source={require('../assets/calendar-text.png')}/>
+			</View>
+			
+			<View style={styles.contenido}>
+			<Text style={styles.etiqueta}>Historial Vacio</Text>
+			</View>
+			<View style={styles.contenedorBtn}>
+			<TouchableOpacity style={styles.boton} >
+				<Text style={styles.etiquetaBtn}>Realizar pedido</Text>
+			</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+		etiqueta:{
+			fontSize:45,
+			textAlign:'center',color:'#fff'
+		},iconoContenedor:{
+			padding:75
+		},
+		contenedor:{
+			flex:2,
+			color: '#fff',
+			alignItems:'center',
+			backgroundColor: "#212B46",
+		},contenido:{
+			flex:2
+		},contenedorBtn:{
+			flex:1,
+		}
+		,
+		boton:
+		{
+			backgroundColor:'#3167fd',
+			width:350,
+			padding:25,
+			
+		},
+		etiquetaBtn:{
+			fontSize:16,
+			alignSelf:'center',
+			fontWeight:'bold',
+			color: '#fff'
+		}
+	});
+
+export default HistorialScreen;
