e311d70a950b866efa9451ebabc0778ee175cb16
LiltLopez <111619197+LiltLopez@users.noreply.github.com>
2022-10-14 21:04:23 -0600

    updated with master

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
-import * as eva from "@eva-design/eva";
-import {
-	ApplicationProvider,
-	Button,
-	Layout,
-	Text,
-} from "@ui-kitten/components";
-import React from "react";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import Navigation from './Navigation';
+
 
 export default function App() {
 	return (
-		<ApplicationProvider {...eva} theme={eva.dark}>
-			<Layout style={styles.layout}>
-				<Text category="h1">La Tienda De La Abuela</Text>
-				<Button color="white">¡Empezar!</Button>
-			</Layout>
-		</ApplicationProvider>
-	);
+		<Navigation />
+	)
 }
 
-const styles = StyleSheet.create({
-	layout: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
---
+++ screens/CatalogoScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const CatalogoScreen = () => {
+
+	return (
+		<View>
+			<Text
+				style={{
+					fontSize:30
+					
+				}}
+				>Catalogo</Text>
+		</View>
+	);
+}
+export default CatalogoScreen;
