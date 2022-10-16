0343ad100f609b25cfcb657e9a4e9d503b2a4392
Cesron <cesronvasquez@gmail.com>
2022-10-15 20:16:18 -0600

    Aplicatoin provider added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,13 @@
-import * as eva from '@eva-design/eva';
-import {
-	ApplicationProvider,
-	Button,
-	Layout,
-	Text,
-} from '@ui-kitten/components';
+import { Button, Layout, Text } from '@ui-kitten/components';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 export default function App() {
 	return (
-		<ApplicationProvider {...eva} theme={eva.dark}>
-			<Layout style={styles.layout}>
-				<Text category='h1'>La Tienda De La Abuela</Text>
-				<Button color='white'>¡Empezar!</Button>
-			</Layout>
-		</ApplicationProvider>
+		<Layout style={styles.layout}>
+			<Text category='h1'>La Tienda De La Abuela</Text>
+			<Button color='white'>¡Empezar!</Button>
+		</Layout>
 	);
 }
 
