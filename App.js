0ee46b3d5adbb7cb61ed092eb3fb8041e5ee5ba6
Cesron <cesronvasquez@gmail.com>
2022-11-09 09:43:33 -0600

    Login fixed

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { ApplicationProvider } from '@ui-kitten/components';
 import React from 'react';
 import Navigation from './components/Navigation';
 import { AuthContext, AuthContextProvider } from './context/AuthContext';
+import { LoginScreen } from './screens';
 
 export default function App() {
 	return (
 		<ApplicationProvider {...eva} theme={eva.dark}>
 			<AuthContextProvider>
 				<AuthContext.Consumer>
-					{({ logged }) => (!logged ? <LoginStack /> : <Navigation />)}
+					{({ logged }) => (!logged ? <LoginScreen /> : <Navigation />)}
 				</AuthContext.Consumer>
 			</AuthContextProvider>
 		</ApplicationProvider>
