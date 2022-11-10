55282308506dc390d291f9dfe014898e54ca1fab
Alejandro Pacheco <pacheco29alejandro@gmail.com>
2022-11-09 23:35:47 -0600

    Commit

--- App.js
+++ App.js
@@ -4,14 +4,22 @@ import React from 'react';
 import LoginStack from './components/LoginStack';
 import Navigation from './components/Navigation';
 import { AuthContext, AuthContextProvider } from './context/AuthContext';
+<<<<<<< Updated upstream
 import { LoginScreen } from './screens';
+=======
+import { PerfilScreen } from './screens/PerfilScreen';
+>>>>>>> Stashed changes
 
 export default function App() {
 	return (
 		<ApplicationProvider {...eva} theme={eva.dark}>
 			<AuthContextProvider>
 				<AuthContext.Consumer>
+<<<<<<< Updated upstream
 					{({ logged }) => (!logged ? <LoginScreen /> : <Navigation />)}
+=======
+					{({ logged }) => (logged ? <LoginStack /> : <Navigation />)}
+>>>>>>> Stashed changes
 				</AuthContext.Consumer>
 			</AuthContextProvider>
 		</ApplicationProvider>
